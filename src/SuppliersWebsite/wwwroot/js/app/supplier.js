3cab034eb02b770b6d17532b0467e10b7e93dcca
Bilal Ahmed <bah@progressive.dk>
2016-04-27 17:22:24 +0200

    Reinventing the wheel. Again.

---
+++ src/SuppliersWebsite/wwwroot/js/app/supplier.js
@@ -0,0 +1,27 @@
+﻿(function() {
+    "use strict";
+
+    angular
+        .module("app")
+        .controller("Supplier", Supplier);
+
+    Supplier.$inject = ["$location"];
+
+    function Supplier($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = "supplier";
+        vm.products = [
+            { "EAN": "1", "Name": "One", "Price": "100$" },
+            { "EAN": "2", "Name": "Two", "Price": "101$" },
+            { "EAN": "3", "Name": "Three", "Price": "102$" },
+            { "EAN": "4", "Name": "Four", "Price": "103$" },
+            { "EAN": "5", "Name": "Five", "Price": "104$" },
+            { "EAN": "6", "Name": "Six", "Price": "105$" },
+            { "EAN": "7", "Name": "Seven", "Price": "106$" }
+        ];
+        activate();
+
+        function activate() {}
+    }
+})();
---
+++ src/SuppliersWebsite/wwwroot/js/app/supplierFactory.js
@@ -0,0 +1,22 @@
+﻿(function() {
+    "use strict";
+
+    angular
+        .module("app")
+        .factory("supplierFactory", supplierFactory);
+
+    supplierFactory.$inject = ["$http"];
+
+    function supplierFactory($http) {
+        var service = {
+            getData: getData
+        };
+
+        function getData() {
+            return $http.get("/api/people");
+            
+        }
+
+        return service;
+    }
+})();
